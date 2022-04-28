a43efd20fce4d19004716cc89abed84e7a5de43d
Humohoja <ac1627061@akfauniversity.org>
2022-04-28 12:44:19 +0500

    comment
    iOS Developing

---
+++ humo/humo/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  humo
+//
+//  Created by KHUMOYUNMIRZO TURSUNPULATOV on 28/04/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
